b543e1ac9c92048413fc8c387e7a2c44586aed06
LovelyCane <cane.sama.lovely@gmail.com>
2025-02-20 21:54:51 +0800

    RenderUtil and SelfTeleport Renderer

--- src/main/java/org/academy/internal/common/ability/builtin/teleporter/skills/SelfTeleport.java
+++ src/main/java/org/academy/internal/common/ability/builtin/teleporter/skills/SelfTeleport.java
@@ -1,11 +1,10 @@
 package org.academy.internal.common.ability.builtin.teleporter.skills;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderBuffers;
-import org.academy.AcademyCraft;
+import net.minecraft.world.phys.AABB;
 import org.academy.api.client.input.InputSystem;
 import org.academy.api.client.render.AcademyCraftRenderSystem;
 import org.academy.api.client.util.RenderUtil;
@@ -18,7 +17,7 @@ public final class SelfTeleport extends Skill {
     public static final Skill INSTANCE = new SelfTeleport();
 
     private SelfTeleport() {
-        super("self_teleport", 1);
+        super("self_teleport", 2);
     }
 
     @Override
@@ -26,14 +25,12 @@ public final class SelfTeleport extends Skill {
         Runnable start = new Runnable() {
             @Override
             public void run() {
-                AcademyCraft.LOGGER.info("SelfTeleport started");
                 AcademyCraftRenderSystem.RENDERER_LIST.add(Client.RENDERER);
             }
         };
         Runnable stop = new Runnable() {
             @Override
             public void run() {
-                AcademyCraft.LOGGER.info("SelfTeleport stopped");
                 AcademyCraftRenderSystem.RENDERER_LIST.remove(Client.RENDERER);
             }
         };
@@ -45,26 +42,20 @@ public final class SelfTeleport extends Skill {
     private static final class Client {
         public static final Minecraft mc = Minecraft.getInstance();
         public static final AcademyCraftRenderSystem.Renderer RENDERER = (poseStack, f, l, bl, camera, gameRenderer, lightTexture, matrix4f, ci) -> {
-            AcademyCraft.LOGGER.info("SelfTeleport client");
             poseStack.pushPose();
 
             final float RAY_DISTANCE = 10f;
 
+            // 准心前方 10 格
             RenderUtil.translateToForward(poseStack, mc.player, RAY_DISTANCE);
 
             final RenderBuffers renderBuffers = mc.renderBuffers();
-            final VertexConsumer buffer = renderBuffers.bufferSource().getBuffer(RenderUtil.GLOWING_CYLINDER);
 
-            RenderUtil.RayRenderer.renderRay(
-                    poseStack,
-                    buffer,
-                    1f, 0.5f, 0,    // 颜色参数（R, G, B）
-                    1f,             // 透明度
-                    0, 50,          // 高度范围
-                    0.125f,         // 半径
-                    32               // 分段精度
-            );
-
+            // 到方框的中心
+            poseStack.translate(-0.5f, -1f, -0.5f);
+            // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            RenderUtil.BoxRenderer.renderWireframeBox(poseStack, renderBuffers.bufferSource(), new AABB(0, 0, 0, 1, 2, 1), 1f, 1f, 1f, 1f);
+            //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
             poseStack.popPose();
         };
     }
