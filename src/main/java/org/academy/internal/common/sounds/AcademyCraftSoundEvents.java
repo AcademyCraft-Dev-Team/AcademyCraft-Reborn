b0a8e6d1e3cc8b2bcf8343b9d00a62d9bbf13608
LovelyCane <cane.sama.lovely@gmail.com>
2025-02-23 21:14:59 +0800

    Sounds

--- src/main/java/org/academy/internal/common/sounds/AcademyCraftSoundEvents.java
+++ src/main/java/org/academy/internal/common/sounds/AcademyCraftSoundEvents.java
@@ -10,9 +10,13 @@ import java.util.List;
 public class AcademyCraftSoundEvents {
     public static final List<SoundEvent> SOUND_EVENT_LIST = new ArrayList<>();
     public static final SoundEvent COIN = SoundEvent.createVariableRangeEvent(new ResourceLocation(AcademyCraft.MOD_ID,"coin"));
+    public static final SoundEvent RAILGUN = SoundEvent.createVariableRangeEvent(new ResourceLocation(AcademyCraft.MOD_ID,"railgun"));
+    public static final SoundEvent ARC_WEAK = SoundEvent.createVariableRangeEvent(new ResourceLocation(AcademyCraft.MOD_ID,"arc_weak"));
 
     static {
         SOUND_EVENT_LIST.add(COIN);
+        SOUND_EVENT_LIST.add(RAILGUN);
+        SOUND_EVENT_LIST.add(ARC_WEAK);
     }
 
     private AcademyCraftSoundEvents() {
