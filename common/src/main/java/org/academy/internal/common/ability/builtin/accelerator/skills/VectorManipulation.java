207767c29506174bb2c1f85d51b313954b29ea2f
LovelyCane <cane.sama.lovely@gmail.com>
2025-03-29 22:29:57 +0800

    Update

--- common/src/main/java/org/academy/internal/common/ability/builtin/accelerator/skills/VectorManipulation.java
+++ common/src/main/java/org/academy/internal/common/ability/builtin/accelerator/skills/VectorManipulation.java
@@ -1,11 +1,16 @@
 package org.academy.internal.common.ability.builtin.accelerator.skills;
 
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.HitResult;
 import org.academy.AcademyCraft;
 import org.academy.AcademyCraftClient;
 import org.academy.api.client.config.SkillClientConfig;
@@ -23,7 +28,8 @@ import java.util.*;
 
 public class VectorManipulation extends Skill {
     public static final VectorManipulation INSTANCE = new VectorManipulation();
-    public static final String KEY_NAME = "vec_manipulation.reflection";
+    public static final String KEY_NAME_REFLECTION = "vec_manipulation.reflection";
+    public static final String KEY_NAME_REVERSE_BLOODFLOW = "vec_manipulation.reverse_bloodflow";
 
     public VectorManipulation() {
         super("vec_manipulation", 1);
@@ -32,7 +38,7 @@ public class VectorManipulation extends Skill {
     @Override
     public void initClient() {
         AcademyCraftClient.CLIENT_CONFIG.getSkillClientConfig(INSTANCE.name, Client.CONFIG);
-        InputSystem.addKeyBinding(KEY_NAME, Client.CONFIG.getKeyBinding(KEY_NAME,
+        InputSystem.addKeyBinding(KEY_NAME_REFLECTION, Client.CONFIG.getKeyBinding(KEY_NAME_REFLECTION,
                         new InputSystem.InputPair(
                                 InputSystem.InputType.KEYBOARD,
                                 new InputSystem.InputEvent(
@@ -43,17 +49,41 @@ public class VectorManipulation extends Skill {
                         )
                 ), Client::toggleReflection
         );
+        InputSystem.addKeyBinding(KEY_NAME_REVERSE_BLOODFLOW, Client.CONFIG.getKeyBinding(KEY_NAME_REVERSE_BLOODFLOW,
+                        new InputSystem.InputPair(
+                                InputSystem.InputType.KEYBOARD,
+                                new InputSystem.InputEvent(
+                                        new LinkedHashSet<>(Set.of(GLFW.GLFW_KEY_R)),
+                                        GLFW.GLFW_RELEASE,
+                                        new LinkedHashSet<>(
+                                                GLFW.GLFW_MOD_ALT
+                                        )
+                                )
+                        )
+                ), Client::reverseBloodflow
+        );
     }
 
     @Override
     public void initServer(MinecraftServer server) {
-        NetworkSystemServer.registerC2SPacketHandler(NetworkResourceLocations.C2S_TOGGLE_REFLECTION_PACKET, (listener, packet) -> Server.toggleReflection(listener.player.getUUID()));
+        NetworkSystemServer.registerC2SPacketHandler(NetworkResourceLocations.C2S_TOGGLE_REFLECTION_PACKET,
+                (listener, packet) ->
+                        Server.toggleReflection(listener.player.getUUID())
+        );
+        NetworkSystemServer.registerC2SPacketHandler(NetworkResourceLocations.C2S_REVERSE_BLOODFLOW,
+                (listener, packet) ->
+                        Server.reverseBloodflow(listener.player)
+        );
     }
 
     public static final class Client {
         public static final SkillClientConfig.SkillClientKeyBindingConfig CONFIG = new VectorManipulationClientConfig();
 
         public static final class VectorManipulationClientConfig extends SkillClientConfig.SkillClientKeyBindingConfig {
+        }
+
+        public static void reverseBloodflow() {
+            NetworkSystemClient.sendPacket(new C2SPacket(NetworkResourceLocations.C2S_REVERSE_BLOODFLOW));
         }
 
         public static void toggleReflection() {
@@ -72,6 +102,20 @@ public class VectorManipulation extends Skill {
             }
         }
 
+        public static void reverseBloodflow(ServerPlayer player) {
+            HitResult hitResult = player.pick(1, 1, false);
+            List<LivingEntity> entityList = player.level().getEntitiesOfClass(LivingEntity.class,
+                    new AABB(new BlockPos((int) hitResult.getLocation().x, (int) hitResult.getLocation().y, (int) hitResult.getLocation().z))
+            );
+            if (!entityList.isEmpty()) {
+                LivingEntity livingEntity = entityList.get(0);
+                if (livingEntity != player) {
+                    livingEntity.hurt(new DamageSource(player.damageSources().damageTypes.getHolderOrThrow(DamageTypes.MAGIC)), livingEntity.getHealth());
+
+                }
+            }
+        }
+
         public static Pair<Boolean, Float> handleHurt(Player player, DamageSource damageSource, float amount) {
             if (player instanceof LocalPlayer) {
                 throw new RuntimeException("WTF? What are you doing? Don't invoke hurt in client!");
@@ -81,12 +125,12 @@ public class VectorManipulation extends Skill {
             }
             float computingPower = AbilitySystemServer.getPlayerComputingPower(player.getUUID());
             float needComputingPower = amount * 10;
+            AcademyCraft.LOGGER.info(damageSource.toString() + damageSource.type());
+            AcademyCraft.LOGGER.info(needComputingPower + " " + computingPower);
             if (computingPower > needComputingPower) {
                 AbilitySystemServer.setPlayerComputingPower(player.getUUID(), computingPower - needComputingPower);
                 Entity source = damageSource.getEntity();
                 Entity directEntity = damageSource.getDirectEntity();
-                AcademyCraft.LOGGER.info(damageSource.toString() + damageSource.type());
-                AcademyCraft.LOGGER.info(needComputingPower + " " + computingPower);
                 if (source != player) {
                     if (damageSource.is(DamageTypes.ARROW) || damageSource.is(DamageTypes.THROWN)) {
                         if (directEntity != null) {
