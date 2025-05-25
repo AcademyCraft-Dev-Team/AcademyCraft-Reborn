8a8101275e13d6b8fddb5a9cf256c1f877e16173
LovelyCane <cane.sama.lovely@gmail.com>
2025-05-25 13:52:40 +0800

    A?
    Signed-off-by: LovelyCane <cane.sama.lovely@gmail.com>

--- common/src/main/java/org/academy/api/common/network/asm/InstancePacketListener.java
+++ common/src/main/java/org/academy/api/common/network/asm/InstancePacketListener.java
@@ -1,10 +1,10 @@
-package org.academy.api.common.network;
+package org.academy.api.common.network.asm;
 
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
-public abstract class InstancePacketHandler implements PacketHandler {
-    private final Object instance;
+public abstract class InstancePacketListener implements IPacketListener {
+    protected final Object instance;
 
-    protected InstancePacketHandler(Object instance) {
+    protected InstancePacketListener(Object instance) {
         if (instance == null) {
             throw new IllegalArgumentException("Instance cannot be null for InstancePacketHandler");
         }
