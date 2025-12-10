325ad281fe3c883eb5ce413d7a181a87ea8fc4c6
ExtremeNovaIX <3177097870@qq.com>
2025-12-10 16:14:24 +0800

    skilldata

--- src/main/java/org/academy/internal/common/skilldata/SkillData.java
+++ src/main/java/org/academy/internal/common/skilldata/SkillData.java
@@ -4,24 +4,44 @@ import com.google.gson.annotations.SerializedName;
 import net.minecraft.resources.Identifier;
 
 public abstract class SkillData {
-        @SerializedName("exp")
-        public float exp;
+    @SerializedName("exp")
+    public float exp;
 
-        public SkillData() {
-            this.exp = 0;
-        }
+    @SerializedName("maxExp")
+    public int maxExp = 200;
 
-        public SkillData(float exp) {
-            this.exp = exp;
-        }
-
-        public float getExp() {
-            return exp;
-        }
-
-        public void setExp(float exp) {
-            this.exp = exp;
-        }
-
-        public abstract Identifier getType();
+    public SkillData() {
+        this.exp = 0;
     }
+
+    public SkillData(float exp) {
+        this.exp = exp;
+    }
+
+    public SkillData(float exp, int maxExp) {
+        this.exp = exp;
+        this.maxExp = maxExp;
+    }
+
+    public boolean isMaxExp() {
+        return exp >= maxExp;
+    }
+
+    public int getMaxExp() {
+        return maxExp;
+    }
+
+    public void setMaxExp(int maxExp) {
+        this.maxExp = maxExp;
+    }
+
+    public float getExp() {
+        return exp;
+    }
+
+    public void setExp(float exp) {
+        this.exp = exp;
+    }
+
+    public abstract Identifier getType();
+}
