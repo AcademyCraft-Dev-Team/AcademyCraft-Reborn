bc8b024c77120102a8c556404084a6256bb00fb3
LovelyCane <cane.sama.lovely@gmail.com>
2025-12-28 18:21:54 +0800

    jbr

--- src/main/java/org/academy/api/client/util/ClientUtil.java
+++ src/main/java/org/academy/api/client/util/ClientUtil.java
@@ -1,6 +1,8 @@
 package org.academy.api.client.util;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.input.InputWithModifiers;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.util.thread.ReentrantBlockableEventLoop;
 import org.academy.api.client.ability.AbilitySystemClient;
@@ -12,6 +14,10 @@ public final class ClientUtil {
     }
 
     public static boolean hasScreen() {
+        return Minecraft.getInstance().screen != null;
+    }
+
+    public static boolean hasNoScreen() {
         return Minecraft.getInstance().screen != null;
     }
 
@@ -28,7 +34,9 @@ public final class ClientUtil {
     }
 
     public static void playDownSound() {
-        Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.SELECT, 1.0F));
+        Minecraft.getInstance().getSoundManager().play(
+                SimpleSoundInstance.forUI(SoundEvents.SELECT, 1.0F)
+        );
     }
 
     public static ReentrantBlockableEventLoop<?> getRenderEventLoop() {
@@ -37,5 +45,28 @@ public final class ClientUtil {
 
     public static ReentrantBlockableEventLoop<?> getMainEventLoop() {
         return Minecraft.getInstance();
+    }
+
+    public static boolean isControlKey(
+            @InputConstants.Value int key, int scancode, @InputWithModifiers.Modifiers int modifiers
+    ) {
+        var options = Minecraft.getInstance().options;
+        var event = new net.minecraft.client.input.KeyEvent(key, scancode, modifiers);
+        var isHotbarKey = false;
+        for (var hotbarKey : options.keyHotbarSlots) {
+            if (hotbarKey.matches(event)) {
+                isHotbarKey = true;
+                break;
+            }
+        }
+        var isMovementKey
+                = options.keyUp.matches(event)
+                || options.keyDown.matches(event)
+                || options.keyLeft.matches(event)
+                || options.keyRight.matches(event)
+                || options.keyJump.matches(event)
+                || options.keyShift.matches(event)
+                || options.keySprint.matches(event);
+        return isHotbarKey || isMovementKey;
     }
 }
