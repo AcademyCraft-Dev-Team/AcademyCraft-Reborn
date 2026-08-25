2807fa2b341868ac3435da13c6562e7f3f578f41
yumeDusk <66866443+yumeDusk@users.noreply.github.com>
2026-08-25 22:59:41 +0800

    Fix resource pack music metadata parsing

--- src/main/kotlin/org/academy/internal/client/app/music/data/MusicData.kt
+++ src/main/kotlin/org/academy/internal/client/app/music/data/MusicData.kt
@@ -1,3 +1,11 @@
 package org.academy.internal.client.app.music.data
 
-data class MusicData(val icon: String, val sourceType: String, val source: String, val subtitle: String)
+import com.google.gson.annotations.SerializedName
+
+data class MusicData(
+    val icon: String,
+    @SerializedName(value = "source_type", alternate = ["sourceType"])
+    val sourceType: String,
+    val source: String,
+    val subtitle: String
+)
