fc080b2330b1bed18d73fe5290e12a418b42cacd
yumeDusk <66866443+yumeDusk@users.noreply.github.com>
2026-09-02 21:22:10 +0800

    扩展HUD布局注册与终端应用排序

---
+++ src/main/java/org/academy/api/client/hud/HudAnchor.java
@@ -0,0 +1,12 @@
+package org.academy.api.client.hud;
+
+/**
+ * Screen anchor used by an externally registered HUD layout region.
+ */
+public enum HudAnchor {
+    TOP_LEFT,
+    TOP_RIGHT,
+    CENTER_LEFT,
+    CENTER_RIGHT,
+    CENTER
+}
